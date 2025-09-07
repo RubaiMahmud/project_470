3bd596ce51cd738c85a1b84ca1f020e3afcd4ff9
Rubai Mahmud <rubaimahmud@Rubais-MacBook-Air.local>
2025-09-08 01:29:19 +0600

    hosting fix

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 class Config:
-    SECRET_KEY = 'secret'
+    SECRET_KEY = os.getenv('SECRET_KEY', 'secret')
     ALLOWED_EXTENSIONS = {'mp3', 'flac', 'wav'}
     ALLOWED_IMAGE_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'webp', 'svg'}
-    MONGO_URI = 'mongodb+srv://rubai535:r1uibiai1535#@jambiplayer.e7cle9o.mongodb.net/?retryWrites=true&w=majority&appName=jambiplayer&tlsAllowInvalidCertificates=true'
+    MONGO_URI = os.getenv('MONGO_URI')
     MONGO_DB_NAME = 'music_app'
